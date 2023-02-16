a9cedd4bfa9994fdae06c924edfdab7cdb966b45
juliantom <torresjulian125@gmail.com>
2023-02-16 15:49:06 -0500

    Update

--- 99-Scripts/02-Retrieve_taxon_specific_sequences.sh
+++ 99-Scripts/02-Retrieve_taxon_specific_sequences.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
 # Run
+
+#workDir=$1
+workDir=2023-Tongue_16S_rRNA
+# folder for original files from HOMD
+dirOriginal=$workDir/01-Original_data
+fileSeqID=$dirOriginal/HOMD_16S_rRNA_RefSeq_V15.22-2023_02_16.fasta
+dirTaxaFasta=$workDir/02-Taxa_fasta
+
+mkdir $dirTaxaFasta
+
+taxonName=Lachnospiraceae
+myTaxonArray=("Butyrivibrio" "Catonella" "Johnsonella" "Lachnoanaerobaculum" "Lachnospir" "Oribacterium" "Shuttleworthia" "Stomatobaculum" "naviforme")
+
+for spGroup in ${myTaxonArray[@]}; do
+  grep -A1 "$spGroup" $fileSeqID | grep -v '^-' >>! $dirTaxaFasta/${taxonName}-HOMD_16S_rRNA.fa
+done
+
+taxonName=Leptotrichia
+myTaxonArray=("Leptotrichia")
+
+for spGroup in ${myTaxonArray[@]}; do
+  grep -A1 "$spGroup" $fileSeqID | grep -v '^-' >>! $dirTaxaFasta/${taxonName}-HOMD_16S_rRNA.fa
+done
+
+taxonName=Veillonella
+myTaxonArray=("Veillonella")
+
+for spGroup in ${myTaxonArray[@]}; do
+  grep -A1 "$spGroup" $fileSeqID | grep -v '^-' >>! $dirTaxaFasta/${taxonName}-HOMD_16S_rRNA.fa
+done
