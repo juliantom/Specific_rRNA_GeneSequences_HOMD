2b58d8e1967096d23e82f5f7910874d7d168862a
juliantom <torresjulian125@gmail.com>
2023-02-16 14:27:10 -0500

    Update

--- 99-Scripts/01-Download_files-HOMD.sh
+++ 99-Scripts/01-Download_files-HOMD.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-# Run 
+# Run
+
+#workDir=$1
+workDir=2023-Tongue_16S_rRNA
+
+# folder for original files from HOMD
+dirOriginal=$workDir/01-Original_data
+
+mkdir $dirOriginal
+wget https://www.homd.org/ftp/16S_rRNA_refseq/HOMD_16S_rRNA_RefSeq/current/seqid_info.txt -P $dirOriginal
+
+myDate=$( date '+%Y_%m_%d' )
+
+mv $dirOriginal/seqid_info.txt $dirOriginal/seqid_info-$myDate.txt
+
+wget https://www.homd.org/ftp/16S_rRNA_refseq/HOMD_16S_rRNA_RefSeq/current/HOMD_16S_rRNA_RefSeq_V15.22.fasta -P $dirOriginal
+
+myDate=$( date '+%Y_%m_%d' )
+
+mv $dirOriginal/HOMD_16S_rRNA_RefSeq_V15.22.fasta $dirOriginal/HOMD_16S_rRNA_RefSeq_V15.22-$myDate.fasta
